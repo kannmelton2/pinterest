1ca59ee47fc169da226a2c05fa778bed96651b8b
Kayla Melton <kannmelton2@gmail.com>
2020-03-25 21:38:33 -0500

    authentication setup completed

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,17 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+
+import auth from './components/auth/auth';
+import authData from './helpers/data/authData';
+import logout from './components/logout/logout';
+
 import '../styles/main.scss';
 
-console.error('hi');
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStatus();
+  auth.loginButton();
+  logout.logoutEvent();
+};
+
+init();
