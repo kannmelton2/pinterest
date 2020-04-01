fe184506a0a7cf83d9a225303b721be1a60adf23
Kayla Melton <kannmelton2@gmail.com>
2020-04-01 16:18:20 -0500

    wip getting correct pins

--- src/javascripts/components/singleBoard/singleBoard.js
+++ src/javascripts/components/singleBoard/singleBoard.js
@@ -1,9 +1,19 @@
-// import pinData from '../../helpers/data/pinData';
+import pinData from '../../helpers/data/pinData';
 // import utils from '../../helpers/utils';
+
 
 const singleBoardView = (e) => {
   const boardId = e.target.closest('.card').id;
-  console.error(boardId);
+  pinData.getPins()
+    .then((pins) => {
+      pins.forEach((pin) => {
+        if (pin.boardId === boardId) {
+          console.error('boardId', boardId, 'pin', pin);
+        }
+      });
+    })
+    .catch((err) => console.error('problem with get pins', err));
+  // console.error('boardId', boardId, 'pins', pins);
 };
 
 export default { singleBoardView };
