f772fb829f987fe8bb2bca5756dd0dd95bc4351c
Kayla Melton <kannmelton2@gmail.com>
2020-04-11 11:46:07 -0500

    can add new board

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -7,6 +7,7 @@ const authDiv = $('#auth');
 const boardsDiv = $('#boards');
 const logoutButton = $('#navbar-logout-btn');
 const homeDiv = $('#home');
+const singleViewDiv = $('#single-view');
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
@@ -15,13 +16,16 @@ const checkLoginStatus = () => {
       authDiv.addClass('hide');
       homeDiv.addClass('hide');
       boardsDiv.removeClass('hide');
+      singleViewDiv.removeClass('hide');
       logoutButton.removeClass('hide');
       boards.buildBoards();
+      boards.boardEvents();
     } else {
       // person is not logged in
       authDiv.removeClass('hide');
       homeDiv.removeClass('hide');
       boardsDiv.addClass('hide');
+      singleViewDiv.addClass('hide');
       logoutButton.addClass('hide');
     }
   });
